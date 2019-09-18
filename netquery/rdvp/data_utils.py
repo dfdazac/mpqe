cf6e1e5157299b66bedf4e336340cb2a8870894c
Daniel <dfdazac@gmail.com>
2019-09-18 18:48:55 +0200

    Initial implementation of entity-types extraction with csv files

--- netquery/rdvp/data_utils.py
+++ netquery/rdvp/data_utils.py
@@ -1,10 +1,10 @@
 from collections import defaultdict, namedtuple
 from torch_geometric.datasets import Entities
 from sacred import Experiment
+import os
 import os.path as osp
 import gzip
 import rdflib as rdf
-from urllib import parse
 import pickle
 
 
@@ -18,12 +18,6 @@ class RDVPDataset(Entities):
         pass
 
 
-DATASET_TYPES = {'AIFB': ['http://swrc.ontoware.org/ontology#Publication',
-                          'http://swrc.ontoware.org/ontology#Person',
-                          'http://swrc.ontoware.org/ontology#Topic',
-                          'http://swrc.ontoware.org/ontology#Organization']}
-
-
 ex = Experiment()
 
 # TODO: Consider using a regular Python CLI
@@ -33,24 +27,28 @@ def config():
     name = 'AIFB'
 
 
-def extract_entities(graph: rdf.Graph, name):
-    query_str = """
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        SELECT ?entity
-        WHERE {{
-            ?entity rdf:type <{0}>
-        }}"""
-
+def extract_entity_types(types_folder):
     node_maps = {}
-    types = DATASET_TYPES[name]
-    for ent_type in types:
-        node_maps[ent_type] = set()
-        query = query_str.format(ent_type)
-        query_results = graph.query(query)
-        for row in query_results:
-            node_maps[ent_type].add(row.entity)
+
+    for file in os.listdir(types_folder):
+        if file.endswith('.csv'):
+            ent_type = file.split('.')[0]
+            file_path = osp.join(types_folder, file)
+            node_maps[ent_type] = set(open(file_path).read().splitlines())
+
+    # TODO: assert that all sets are disjoint
 
     return node_maps
+
+
+def get_entity_type(node_maps, entity):
+    ent_type = None
+    for t in node_maps:
+        if entity in node_maps[t]:
+            ent_type = t
+            break
+
+    return ent_type
 
 
 @ex.command(unobserved=True)
@@ -58,13 +56,13 @@ def extract_graph_data(data_dir, name):
     # Load graph
     data_path = osp.join(data_dir, name)
     dataset = RDVPDataset(data_path, name)
-
     graph_file, *_ = dataset.raw_paths
     graph = rdf.Graph()
     with gzip.open(graph_file, 'rb') as f:
         graph.parse(file=f, format='nt')
 
-    node_maps = extract_entities(graph, name)
+    # Extract entities of predefined types, listed in .csv files
+    node_maps = extract_entity_types(osp.join(data_path, 'processed'))
 
     entity_ids = defaultdict(lambda: len(entity_ids))
     entity_types = dict()
@@ -75,27 +73,23 @@ def extract_graph_data(data_dir, name):
 
     Entity = namedtuple('Entity', ['id', 'ent_type'])
 
-    print('Processing graph...')
+    print('Extracting triples...')
     for subj, pred, obj in graph.triples((None, None, None)):
-        # print('', subj, '\n', pred, '\n', obj, '\n' + '-'*100)
-        rel = str(pred)
-        relations.add(rel)
+        subj_type = get_entity_type(node_maps, str(subj))
+        obj_type = get_entity_type(node_maps, str(obj))
 
-        entities = []
-        for entity in (subj, obj):
-            # Determine entity ID and type
-            id = entity_ids[entity]
-            if id not in entity_types:
-                entity_types[id] = get_entity_type(str(entity))
-            ent_type = entity_types[id]
-            entities.append(Entity(id, ent_type))
+        # Add only triples involving extracted entities (i.e. with known type)
+        if all([subj_type, obj_type]):
+            # FIXME: keys in node_maps should be int IDs, not strings!
+            subject_ent = Entity(entity_ids[subj], subj_type)
+            object_ent = Entity(entity_ids[obj], obj_type)
 
-            node_maps[ent_type].add(id)
+            rel = str(pred)
+            relations.add(rel)
 
-        subject_ent, object_ent = entities
-        rels[subject_ent.ent_type].add((object_ent.ent_type, rel))
-        triple = (subject_ent.ent_type, rel, object_ent.ent_type)
-        adj_lists[triple][subject_ent.id].add(object_ent.id)
+            rels[subject_ent.ent_type].add((object_ent.ent_type, rel))
+            triple = (subject_ent.ent_type, rel, object_ent.ent_type)
+            adj_lists[triple][subject_ent.id].add(object_ent.id)
 
     # Convert rels to dict of list
     rels = {ent_type: list(rels[ent_type]) for ent_type in rels.keys()}
