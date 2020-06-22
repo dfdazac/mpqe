6a269dbce50c5ab37b5a91d7f235054d3e3d08f6
Daniel <dfdazac@gmail.com>
2020-06-22 14:57:05 +0200

    Remove old bio folder

--- mpqe/bio/data_utils.py
+++
@@ -1,8 +0,0 @@
-import mpqe.data_utils as utils
-
-# Make training data for Bio
-utils.make_train_test_edge_data("/dfs/scratch0/nqe-bio/")
-utils.make_train_queries("/dfs/scratch0/nqe-bio/")
-utils.make_test_queries("/dfs/scratch0/nqe-bio/")
-utils.clean_test_queries("/dfs/scratch0/nqe-bio/")
-
